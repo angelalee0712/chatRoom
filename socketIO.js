22b38e6bc8c79809be509d31a80f6169e8e112c6
lqllyf <lqllyf1314@163.com>
2016-01-13 21:42:59 +0800

    this is the UI of the chatroom, and we will add the bachround later

---
+++ socketIO.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2015/11/4.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
